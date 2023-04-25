0d49b0302583cef8528f80eb3d3cf21fb49de005
romizka <awencka1@gmail.com>
2023-04-25 21:08:12 +0200

    add task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const emailEl = document.querySelector("input[name='email'");
+const messageEl = document.querySelector("textarea[name='message'");
+
+const saveData = () => {
+  const data = {
+    email: emailEl.value,
+    message: messageEl.value,
+  };
+
+  localStorage.setItem('feedback-form-state', JSON.stringify(data));
+};
+
+const updateData = () => {
+  const data = JSON.parse(localStorage.getItem('feedback-form-state'));
+  if (data) {
+    emailEl.value = data.email || '';
+    messageEl.value = data.message || '';
+  }
+};
+
+window.addEventListener('load', updateData);
+
+emailEl.addEventListener('input', throttle(saveData, 500));
+messageEl.addEventListener('input', throttle(saveData, 500));
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  const data = {
+    email: emailEl.value,
+    message: messageEl.value,
+  };
+  console.log('Data:', data);
+  localStorage.removeItem('feedback-form-state');
+  emailEl.value = '';
+  messageEl.value = '';
+});
