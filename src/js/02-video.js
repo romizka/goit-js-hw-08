b9a848ae215b6d36797813fa5c78a1891759ab07
romizka <awencka1@gmail.com>
2023-04-25 18:06:33 +0200

    update task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const saveTime = data =>
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+
+const saveTimeDelay = throttle(saveTime, 1000);
+
+const getCurrentTime = localStorage.getItem('videoplayer-current-time');
+
+player.setCurrentTime(getCurrentTime);
+player.on('timeupdate', saveTimeDelay);
