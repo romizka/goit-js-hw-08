933f7edb93f1a47ede1de468d844f997413126f5
romizka <awencka1@gmail.com>
2023-04-25 15:02:33 +0200

    add lightbox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,30 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+
+galleryContainer.innerHTML = galleryItems
+  .map(
+    item => `
+<li class="gallery__item">
+<a class="gallery__link" href="${item.original}">
+  <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+</a>
+</li>
+`
+  )
+  .join('');
+
+galleryContainer.addEventListener('click', e => {
+  e.preventDefault();
+});
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionPosition: 'bottom',
+  captionType: 'attr',
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+console.log(galleryItems);
